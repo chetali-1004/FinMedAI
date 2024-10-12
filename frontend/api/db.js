9597e96a2ad0004bea6d3f0c2ab091df6a4ea807
Chetali Goyal <goyalchetali123@gmail.com>
2024-10-12 13:13:36 +0530

    defining database schema

---
+++ frontend/api/db.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://mern-book-store:Jr41tXRsUTBGlF8r@cluster0.mlvki.mongodb.net/bajaj_hackrx"
+  )
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log("mongo conneection failed", err));
+
+const patientSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    max: 120,
+    required: false,
+  },
+  sex: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  diagnoses: {
+    type: [String],
+    required: true,
+  },
+  prescriptions: [
+    {
+      date: {
+        type: String, // Store the date as a string
+        required: false,
+      },
+      extractedDiagnosis: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const diagnosisSchema = mongoose.Schema({
+  provisional_diagnosis: String,
+});
+
+const prescriptionSchema = mongoose.Schema({
+  patient_id: String,
+  diagnosis: [diagnosisSchema],
+  extracted_at: {
+    type: String,
+  },
+});
+
+//indexing the db to reduce complexity and increase speed
+patientSchema.index({ name: 1, age: 1, phone: 1 });
+
+const patient = mongoose.model("patients", patientSchema);
+const prescription = mongoose.model("prescriptions", prescriptionSchema);
+
+module.exports = {
+  patient,
+  prescription,
+};
